6d9ab1ea3159c540313a7e01ca8aa0319ee0126c
rkukuczka <rafal.qqczka@gmail.com>
2025-07-15 15:01:25 +0200

    Add project files.

---
+++ Controllers/ExpensesController.cs
@@ -0,0 +1,58 @@
+﻿using FinanceApp.Data;
+using FinanceApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceApp.Controllers
+{
+    public class ExpensesController : Controller
+    {
+        FinanceAppContext _context;
+        public ExpensesController(FinanceAppContext context)
+        {
+                _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public IActionResult Index()
+        {
+            var expenses = _context.Expenses.ToList();
+
+            return View(expenses);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Movies/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Description, Amount, Category, Date")] Expense expense)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Expenses.Add(expense);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(expense);
+        }
+
+        public IActionResult GetChart()
+        { 
+            var data = _context.Expenses
+                .GroupBy(e => e.Category)
+                .Select(g => new
+                {
+                    Category = g.Key,
+                    TotalAmount = g.Sum(e => e.Amount)
+                })
+                .ToList();
+
+            return Json(data);
+        }
+
+    }
+}
---
+++ Models/Expense.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinanceApp.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
+        public string Description { get; set; } = null!;
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage="Amount should be greater then 0")]
+        public decimal Amount { get; set; }
+        [Required]
+        public string Category { get; set; } = null!;
+        public DateTime Date { get; set; } = DateTime.Now;
+
+    }
+}
